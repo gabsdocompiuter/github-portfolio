e63fc06e5b641f6cac793e2b310c0c3c6eace8b0
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-05-24 14:45:21 -0300

    Adiciona imagem de perfil do usuário na tela

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
-    
-var url = 'https://api.github.com/users/gmonteeeiro';
+
+let url = 'https://api.github.com/users/gmonteeeiro';
 
 axios.get(url)
-    .then(function(response)
-    {
-        console.log(response);
-    })
-    .catch(function(error)
-    {
-        console.warn(error);
-    })
+    .then(montaPortfolio)
+    .catch((error) => console.warn(error));
+
+function montaPortfolio(response){
+    let user = response.data.login;
+    let name = response.data.name;
+    let avatar = response.data.avatar_url;
+    console.log(avatar);
+
+    let myDiv = document.getElementById('img');
+
+    let imgElement = document.createElement('img');
+    imgElement.setAttribute('src', avatar);
+
+    myDiv.appendChild(imgElement);
+
+
+}
